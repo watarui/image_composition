de7e1741bb7eb53fe52656abb470bcd99913f846
watarui <wataruby@gmail.com>
2019-08-20 18:51:10 +0900

    Flask-WTFによるvalidation, CSRF対策の実装

--- src/image_composition/app.py
+++ src/image_composition/app.py
@@ -1,11 +1,16 @@
 import os
+import re
 from functools import reduce
 from pathlib import Path
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import Flask, render_template, request, redirect, url_for
+from flask_wtf import FlaskForm
+from flask_wtf.csrf import CSRFProtect
+from wtforms import StringField, ValidationError
 from .controller import ImageEditor
 
 app = Flask(__name__)
+csrf = CSRFProtect(app)
 app.config.from_object("src.image_composition.config.Config")
 
 
@@ -41,9 +46,31 @@ def generate(firstname, lastname, scene, output_filename="out"):
     return dst
 
 
+class Form(FlaskForm):
+    firstname = StringField("firstname")
+    lastname = StringField("lastname")
+
+    def validate_firstname(self, firstname):
+        if firstname.data == "":
+            raise ValidationError("名前を入力してください")
+        if len(firstname.data) > 10:
+            raise ValidationError("10文字以内で入力してください")
+        if not re.match("^[A-Z]+$", firstname.data):
+            raise ValidationError("半角アルファベット（大文字）で入力してください")
+
+    def validate_lastname(self, lastname):
+        if lastname.data == "":
+            raise ValidationError("名前を入力してください")
+        if len(lastname.data) > 10:
+            raise ValidationError("10文字以内で入力してください")
+        if not re.match("^[A-Z]+$", lastname.data):
+            raise ValidationError("半角アルファベット（大文字）で入力してください")
+
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    form = Form(request.form)
+    return render_template("index.html", form=form)
 
 
 @app.route("/result", methods=["GET", "POST"])
@@ -51,17 +78,20 @@ def index():
 def result(token=None):
     # /result への初回POST時
     if not token and request.method == "POST":
-        # token 生成
-        s = Serializer(app.config["SECRET_KEY"], app.config["EXPIRES_IN"])
-        token = s.dumps({}).decode("utf-8")
-        # フォーム入力値の取得
-        firstname = request.form["firstname"]
-        lastname = request.form["lastname"]
-        scene = request.form["scene"]
-        # TODO: Add validations
-        generate(firstname, lastname, scene, token)
+        form = Form(request.form)
+        if form.validate_on_submit():
+            # token 生成
+            s = Serializer(app.config["SECRET_KEY"], app.config["EXPIRES_IN"])
+            token = s.dumps({}).decode("utf-8")
+            # フォーム入力値の取得
+            firstname = form.firstname.data
+            lastname = form.lastname.data
+            scene = request.form["scene"]
 
-        return redirect(url_for("result", token=token))
+            generate(firstname, lastname, scene, token)
+
+            return redirect(url_for("result", token=token))
+        return render_template("index.html", form=form)
 
     if not token and request.method == "GET":
         return redirect(url_for("index"))
